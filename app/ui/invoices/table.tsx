4e30b7b0ca920bdb070e67987ae253da09577878
anvuvuong <anvuvuong05@gmail.com>
2024-02-20 18:00:28 +0700

    update before go home

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
 import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
+import { formatDateToLocal, formatCurrency } from '@/app/dashboard/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
 
 export default async function InvoicesTable({
