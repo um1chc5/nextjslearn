91556640532ddc59487efb36c2f6b14ae2560f33
anvuvuong <anvuvuong05@gmail.com>
2024-02-20 15:09:56 +0700

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['vietnamese'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
